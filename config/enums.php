cf1de080a0b923b26867b9094667fcda3d0651be
User <amirbesharati59@gmail.com>
2022-07-13 16:42:48 +0430

    Make status list for available response status in system

--- config/enums.php
+++ config/enums.php
@@ -1,10 +1,14 @@
 <?php
 return [
-    'apiResponseService' => [
-        'statuses' => [
-            'success' => 200 ,
-            'error' => 500 ,
-        ]
+    'response_statuses' => [
+        'success' => 200 , //OK
+        'no_content' => 204 , //No Content
+        'bad_request' => 400 , //Bad request
+        'unauthorized' => 401 , //Unauthorized or unauthenticated
+        'forbidden' => 403 , //Permission denied
+        'not_found' => 404 , //Not found
+        'method_not_allowed' => 405  , //Method not allowed
+        'unprocessable_entity' => 422  , //Method not allowed
     ] ,
 
 
