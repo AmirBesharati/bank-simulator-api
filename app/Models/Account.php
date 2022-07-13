dfe8688ae4ac5ea6224278d3475061cbe7438581
User <amirbesharati59@gmail.com>
2022-07-14 03:10:08 +0430

    Account model created with accountType Relation

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name' , 'account_type_id' , 'balance' , 'number'];
+
+    protected $guarded = ['balance'];
+
+    public function accountType(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(AccountType::class);
+    }
 }
