210492bf317bb59d2590c58a98f958d3e069b30f
User <amirbesharati59@gmail.com>
2022-07-13 16:43:57 +0430

    Make messages.php to include all necessary messages from single repository

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'api' => [
+        'auth' => [
+            'unauthenticated' => 'User is unauthenticated' ,
+            'registered' => 'User :attribute registered successfully' ,
+            'loggedIn' => 'User :attribute logged in successfully' ,
+            'loggedInIssue' => 'Cant login user :email' ,
+        ]
+    ] ,
+
+    'validation' => [
+        'message' => 'The given data was invalid'
+    ]
+];
