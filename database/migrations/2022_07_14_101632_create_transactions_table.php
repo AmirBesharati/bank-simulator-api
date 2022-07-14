c2dd5c748d4b02e9965110012319ae4c3f920879
User <amirbesharati59@gmail.com>
2022-07-14 18:26:24 +0430

    receiver_account_id added to transactions table instead of morph relation

--- database/migrations/2022_07_14_101632_create_transactions_table.php
+++ database/migrations/2022_07_14_101632_create_transactions_table.php
@@ -16,16 +16,18 @@ class CreateTransactionsTable extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedSmallInteger('transaction_type_id');
-            $table->unsignedInteger('account_id');
+            $table->unsignedInteger('sender_account_id');
+            $table->unsignedInteger('receiver_account_id');
             $table->text('note')->nullable();
+            $table->unsignedDouble('amount');
             $table->string('reference_code')->unique();
-            $table->morphs('transactionable');
             $table->timestamps();
             $table->softDeletes();
 
 
             $table->foreign('transaction_type_id')->references('id')->on('transaction_types')->onDelete('cascade');
-            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('sender_account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('receiver_account_id')->references('id')->on('accounts')->onDelete('cascade');
         });
     }
 
