774ce1e657357103c5f2fad0ec885b95fd7c2f4b
User <amirbesharati59@gmail.com>
2022-07-14 17:14:52 +0430

    TransactionType and Transaction repositories bound in applicatiopn service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,13 @@ namespace App\Providers;
 
 use App\Repositories\Contracts\AccountRepositoryInterface;
 use App\Repositories\Contracts\AccountTypeRepositoryInterface;
+use App\Repositories\Contracts\TransactionRepositoryInterface;
+use App\Repositories\Contracts\TransactionTypeRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\Eloquent\AccountEloquentRepository;
 use App\Repositories\Eloquent\AccountTypeEloquentRepository;
+use App\Repositories\Eloquent\TransactionEloquentRepository;
+use App\Repositories\Eloquent\TransactionTypeEloquentRepository;
 use App\Repositories\Eloquent\UserEloquentRepository;
 use App\Services\Api\ApiResponseService;
 use Illuminate\Support\ServiceProvider;
@@ -23,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(UserRepositoryInterface::class , UserEloquentRepository::class);
         $this->app->bind(AccountTypeRepositoryInterface::class , AccountTypeEloquentRepository::class);
         $this->app->bind(AccountRepositoryInterface::class , AccountEloquentRepository::class);
+        $this->app->bind(TransactionTypeRepositoryInterface::class , TransactionTypeEloquentRepository::class);
+        $this->app->bind(TransactionRepositoryInterface::class , TransactionEloquentRepository::class);
 
         $this->app->bind('apiResponseService',function(){
             return new ApiResponseService();
